0f9c3179aecbcd0abdec3e26c9314d2162d7ab2e
Jose Esteves de Souza Neto <jose.desouzaneto.ext@dkv-mobility.com>
2020-10-14 12:45:33 +0200

    [Array] insert method

--- src/main/java/us/greatapps4you/cs/array/Array.java
+++ src/main/java/us/greatapps4you/cs/array/Array.java
@@ -2,4 +2,19 @@ package us.greatapps4you.cs.array;
 
 public class Array {
 
+    private int count = 0;
+    private int[] data = new int[3];
+
+    public void insert(int element) {
+        data[count] = element;
+        count++;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public int getAt(int index) {
+        return data[index];
+    }
 }
--- src/test/java/us/greatapps4you/cs/array/ArrayTest.java
+++ src/test/java/us/greatapps4you/cs/array/ArrayTest.java
@@ -7,8 +7,23 @@ import org.junit.jupiter.api.Test;
 class ArrayTest {
 
     @Test
-    void initArrayShorthand(){
-        int[] actual = {10,20,30};
+    void insert() {
+        Array array = new Array();
+        array.insert(10);
+        array.insert(20);
+        array.insert(30);
+
+        int expectedSize = 3;
+
+        Assertions.assertEquals(expectedSize, array.size());
+        Assertions.assertEquals(10, array.getAt(0));
+        Assertions.assertEquals(20, array.getAt(1));
+        Assertions.assertEquals(30, array.getAt(2));
+    }
+
+    @Test
+    void initArrayShorthand() {
+        int[] actual = {10, 20, 30};
         int[] expected = new int[3];
         expected[0] = 10;
         expected[1] = 20;
