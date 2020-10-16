ea01679408a56b9562e14cedd190d72ad17de4c4
Jose Esteves de Souza Neto <jose.desouzaneto.ext@dkv-mobility.com>
2020-10-16 12:03:25 +0200

    [String] String and StringBuffer equality

--- src/test/java/us/greatapps4you/cs/array/StringCasesTest.java
+++ src/test/java/us/greatapps4you/cs/array/StringCasesTest.java
@@ -8,6 +8,32 @@ import org.junit.jupiter.api.Test;
 class StringCasesTest {
 
     @Test
+    void stringBufferComparisonTest(){
+        StringBuffer sb1 = new StringBuffer("durga");
+        StringBuffer sb2 = new StringBuffer("durga");
+
+        Assertions.assertFalse(sb1 == sb2); // Because == compares reference not content
+        Assertions.assertFalse(sb1.equals(sb2)); // Because Object.equals() is meant for reference comparison and StringBuffer does not override it for content comparison.
+    }
+
+    @Test
+    void stringComparisonTest(){
+        String s1 = new String("durga");
+        String s2 = new String("durga");
+
+        Assertions.assertFalse(s1 == s2); // Because == compares reference not content
+        Assertions.assertTrue(s1.equals(s2)); // Because String overrides Object.equals() for content comparison
+    }
+
+    @Test
+    void stringTest(){
+        String s = new String("durga");
+        s.concat(" software"); // A new String Object is created but not assigned to any variable
+        Assertions.assertEquals("durga", s); //Because String is immutable
+        System.out.println(s);
+    }
+
+    @Test
     void stringBufferTest(){
         StringBuffer sb = new StringBuffer("durga");
         sb.append(" software");
