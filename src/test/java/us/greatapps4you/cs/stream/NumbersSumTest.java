2d8487dea5383ca8e260916fa26195f1513e3ac5
Jose Esteves de Souza Neto <jose.desouzaneto.ext@dkv-mobility.com>
2020-10-19 15:00:46 +0200

    [Sum Number List] Sum with Map

--- src/test/java/us/greatapps4you/cs/stream/NumbersSumTest.java
+++ src/test/java/us/greatapps4you/cs/stream/NumbersSumTest.java
@@ -1,7 +1,9 @@
 package us.greatapps4you.cs.stream;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +12,21 @@ import org.junit.jupiter.api.Test;
 class NumbersSumTest {
 
     private List<Integer> numbers;
+    private Map<String, Integer> numbersMap;
 
     @BeforeEach
     void setup() {
         numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        numbersMap = new HashMap<>();
+        numbersMap.put("ONE", 1);
+        numbersMap.put("TWO", 2);
+        numbersMap.put("THREE", 3);
+        numbersMap.put("FOUR", 4);
+        numbersMap.put("FIVE", 5);
+        numbersMap.put("SIX", 6);
+        numbersMap.put("SEVEN", 7);
+        numbersMap.put("EIGHT", 8);
+        numbersMap.put("NINE", 9);
     }
 
     @Test
@@ -43,6 +56,12 @@ class NumbersSumTest {
     @Test
     void sumNumbersWithIntStream() {
         Integer actual = numbers.stream().mapToInt(Integer::intValue).sum();
+        Assertions.assertEquals(45, actual);
+    }
+
+    @Test
+    void sumNumbersWithMap() {
+        Integer actual = numbersMap.values().stream().mapToInt(Integer::valueOf).sum();
         Assertions.assertEquals(45, actual);
     }
 
